ec2c4f78826c2c6427ed4db691c4c752ac8d279f
Henk Boelman <henk@boelman.eu>
2017-05-31 16:53:50 +0200

    Setup Bot

---
+++ AzureChallengeNetwork/AzureChallengeNetwerk.Bot/Dialogs/RootDialog.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Configuration;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.Luis;
+using Microsoft.Bot.Builder.Luis.Models;
+
+namespace AzureChallengeNetwerk.Bot.Dialogs
+{
+
+    public class ConfigLuisModelAttribute : LuisModelAttribute
+    {
+        public ConfigLuisModelAttribute(string modelID, string subscriptionKey)
+            : base(ConfigurationManager.AppSettings[modelID], ConfigurationManager.AppSettings[subscriptionKey])
+        {}
+    }
+
+    [ConfigLuisModel("LUISmodelID", "LUISsubscriptionKey")]
+    [Serializable]
+    public class RootDialog : LuisDialog<object>
+    {
+        
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Hmmmm, I didn't understand that.  I'm still learning!");
+        }
+
+        [LuisIntent("Greeting")]
+        public async Task Greeting(IDialogContext context, LuisResult result)
+        {
+            await context.PostAsync("Hello!  I am a Photo Organization Bot.  I can search your photos, share your photos on Twitter, and order prints of your photos.  You can ask me things like 'find pictures of food'.");
+        }
+
+        [LuisIntent("SearchPics")]
+        public async Task SearchPics(IDialogContext context, LuisResult result)
+        {
+            // Check if LUIS has identified the search term that we should look for.  
+            string facet = null;
+            EntityRecommendation rec;
+            if (result.TryFindEntity("facet", out rec)) facet = rec.Entity;
+
+            // If we don't know what to search for (for example, the user said
+            // "find pictures" or "search" instead of "find pictures of x"),
+            // then prompt for a search term.  
+            if (string.IsNullOrEmpty(facet))
+            {
+                PromptDialog.Text(context, ResumeAfterSearchTopicClarification, "What kind of picture do you want to search for?");
+            }
+            else
+            {
+                await context.PostAsync("Searching pictures...");
+                context.Call(new SearchDialog(facet), ResumeAfterSearchDialog);
+            }
+        }
+
+        private async Task ResumeAfterSearchTopicClarification(IDialogContext context, IAwaitable<string> result)
+        {
+            string searchTerm = await result;
+            context.Call(new SearchDialog(searchTerm), ResumeAfterSearchDialog);
+        }
+
+        private async Task ResumeAfterSearchDialog(IDialogContext context, IAwaitable<object> result)
+        {
+            await context.PostAsync("Done searching pictures");
+        }
+
+    }
+}
---
+++ AzureChallengeNetwork/AzureChallengeNetwerk.Bot/Dialogs/SearchDialog.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureChallengeNetwerk.Bot.Models;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace AzureChallengeNetwerk.Bot.Dialogs
+{
+    [Serializable]
+    public class SearchDialog : IDialog<object>
+    {
+        private readonly string _searchText;
+        private readonly string _searchServiceName;
+        private readonly string _searchServiceApiKey;
+        private readonly string _indexName;
+        private readonly string _imageBaseUrl;
+
+        public SearchDialog(string facet)
+        {
+             _searchText = facet;
+             _searchServiceName = ConfigurationManager.AppSettings["searchServiceName"];
+             _searchServiceApiKey = ConfigurationManager.AppSettings["searchServiceApiKey"];
+             _indexName = ConfigurationManager.AppSettings["indexName"];
+             _imageBaseUrl = ConfigurationManager.AppSettings["imageBaseUrl"];
+        }
+
+        public async Task StartAsync(IDialogContext context)
+        {
+            // Connect to Azure search
+            SearchServiceClient searchClient = new SearchServiceClient(_searchServiceName, new SearchCredentials(_searchServiceApiKey));
+            ISearchIndexClient indexClient = searchClient.Indexes.GetClient(_indexName);
+            DocumentSearchResult<ImageInsight> results = indexClient.Documents.Search<ImageInsight>(_searchText);
+
+            await SendResults(context, results);
+        }
+
+        private async Task SendResults(IDialogContext context, DocumentSearchResult<ImageInsight> results)
+        {
+            // Creating the response message
+            var message = context.MakeMessage();
+
+            if (results.Results.Count == 0)
+            {
+                await context.PostAsync("There were no results found for \"" + _searchText + "\".");
+                context.Done<object>(null);
+            }
+            else
+            {
+                var cards = results.Results.Select(h => new HeroCard
+                {
+                    Title = h.Document.ImageId,
+                    Images = new[] { new CardImage(_imageBaseUrl, h.Document.ImageId) },
+                    Text = h.Document.Caption
+                });
+                message.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                message.Attachments = cards.Select(c => c.ToAttachment()).ToList();
+                message.Text = "Here are the results that I found:";
+
+                await context.PostAsync(message);
+                context.Done<object>(null);
+            }
+        }
+    }
+}
